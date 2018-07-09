13e77f3097c1e5b2a628d1f895f224923219e34e
Yurajun <yurajun@mail.ru>
2018-07-09 21:13:28 +0300

    redux js example

--- app/blocks/wrapper-app/wrapper-app.js
+++ app/blocks/wrapper-app/wrapper-app.js
@@ -1,9 +1,47 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
-import {render}from 'react-dom';
-import App from '../app/App'; // eslint-disable-line no-unused-vars
+// import React from 'react'; // eslint-disable-line no-unused-vars
+// import {render}from 'react-dom';
+// import App from '../app/App'; // eslint-disable-line no-unused-vars
 
 
-render(
-	<App />,
-	document.querySelector('.wrapper-app')
-);
+// render(
+// 	<App />,
+// 	document.querySelector('.wrapper-app')
+// );
+
+import {createStore}from 'redux';
+
+function playList(state = [], action){
+	if (action.type === 'ADD_TRACK'){
+		return [
+			...state,
+			action.payload,
+		];
+	}
+	return state;
+}
+
+const store = createStore(playList);
+
+
+store.subscribe(() => {
+	console.log('subScribe', store.getState());
+	const list = document.querySelector('.list');
+	list.innerHTML = '';
+
+	store.getState().forEach(track => {
+		const li = document.createElement('li');
+		li.textContent = track;
+		list.append(li);
+	});
+
+});
+
+// store.dispatch({type: 'ADD_TRACK', payload: 'Smells like spirit'});
+// store.dispatch({type: 'ADD_TRACK', payload: 'Enter sendmen'});
+
+const addTrackBtn = document.querySelector('.addTrack');
+addTrackBtn.addEventListener('click', () => {
+	const trackName = document.querySelector('.trackInput');
+	store.dispatch({type: 'ADD_TRACK', payload: trackName.value});
+	trackName.value = '';
+});
