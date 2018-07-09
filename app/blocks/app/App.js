79d0909175c0cee5989fa4aeb42a93dfb2e360ea
Yurajun <yurajun@mail.ru>
2018-07-09 23:48:56 +0300

    simple example redux

--- app/blocks/app/App.js
+++ app/blocks/app/App.js
@@ -20,18 +20,20 @@ class App extends Component {
 		const props = this.props;
 		console.log('this.props', props.testStore);
 		return (
-			<div className='container'>
-				<input ref={input => {this.trackInput = input;}} type='text' />
-				<button onClick={this.addTrack.bind(this)}>Add Track</button>
-				<ul>
-					{
-						props.testStore.map((track, index) =>
-							<li key={index}>{track}</li>
-						)
-					}
-				</ul>
-			</div>
-		);
+			<div />
+		); // (
+		// 	<div className='container'>
+		// 		<input ref={input => {this.trackInput = input;}} type='text' />
+		// 		// <button onClick={this.addTrack.bind(this)}>Add Track</button>
+		// 		<ul>
+		// 			{
+		// 				props.testStore.map((track, index) =>
+		// 					<li key={index}>{track}</li>
+		// 				)
+		// 			}
+		// 		</ul>
+		// 	</div>
+		// );
 	}
 
 }
